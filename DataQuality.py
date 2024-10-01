71e7a2897626282569cd55746b90895df35746b2
NicolasSP90 <nicolassp90@hotmail.com>
2024-10-01 10:25:04 -0300

    adicionado IPython.display

--- DataQuality.py
+++ DataQuality.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from IPython.display import display
 
 # Cores
 color_palette = sns.color_palette("dark")
